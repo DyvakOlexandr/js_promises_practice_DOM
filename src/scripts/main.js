d2d5d86a57f6f74f5e081f8e6c2f5b3487c57727
DyvakOleksandr <dyvakaleksandr370@gmail.com>
2025-05-30 10:42:51 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,40 @@
 'use strict';
+
+let isClicked = false;
+
+function createMessage(message) {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.textContent = message;
+
+  if (message.includes('resolved')) {
+    div.classList.add('success');
+  }
+
+  if (message.includes('rejected')) {
+    div.classList.add('error');
+  }
+  document.body.appendChild(div);
+}
+
+const p1 = new Promise((resolve, reject) => {
+  const successMessage = 'First promise was resolved';
+
+  document.addEventListener('click', () => {
+    isClicked = true;
+    resolve(successMessage);
+  });
+
+  setTimeout(() => {
+    if (isClicked === false) {
+      reject(new Error('First promise was rejected'));
+    }
+  }, 3000);
+});
+
+p1.then((message) => {
+  createMessage(message);
+}).catch((error) => {
+  createMessage(error.message);
+});
